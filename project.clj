f53ff4ccd489d13e2c6d8eb4742c975542c61bb8
sprin <sprin>
2013-07-08 21:55:22 -0700

    Initial project.clj

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject cassa-meter-query "0.1.0-SNAPSHOT"
+  :description "A simple web app to serve meter data queries"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring/ring-jetty-adapter "1.2.0"]
+                 [compojure "1.1.5"]]
+  :main cassa-meter-query.core)
